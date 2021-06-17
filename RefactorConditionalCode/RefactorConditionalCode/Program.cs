605de9b8a3c64efc4bccced9bdd060beb7f95634
Rachit Srivastava <rachitsrivastava77@gmail.com>
2021-06-17 11:33:40 +0530

    Refactored If-Else Code Using Polymorphism

---
+++ RefactorConditionalCode/RefactorConditionalCode/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace RefactorConditionalCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        #region Problem Statement
+        public void FlattenXml(XElement xml, string id = null)
+        {
+            var elements = xml.Elements().ToList();
+            foreach (var element in elements)
+            {
+                //if (element.Name == "name1")
+                //{
+                //    Func1();
+                //}
+                //if (element.Name == "name2")
+                //{
+                //    Func2();
+                //}
+                //if (element.Name == "name3")
+                //{
+                //    DoSomethingElse();
+                //    FlattenXml(content, tempId);
+                //    Func3();
+                //}
+                //else
+                //{
+                //    DoSomethingCompletelyDifferent();
+                //    FlattenXml(element, id);
+                //}
+            }
+            xml.Elements("name3").Remove();
+        }
+
+        #endregion
+    }
+}
---
+++ RefactorConditionalCode/RefactorConditionalCode/RefactoredCode.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorConditionalCode
+{
+    class RefactoredCode
+    {
+        static void Main(string[] args)
+        {
+            var operationContext = new OperationContext();
+            //var elements = xml.Elements().ToList();
+            //foreach (var element in elements)
+            //{
+            //    operationContext.GetOperationData(element.Name, element);
+            //}
+        }
+    }
+
+    public interface ISomeOperation
+    {
+        string DoOperation(string data);
+    }
+
+    public class OperationA : ISomeOperation
+    {
+        public string DoOperation(string data)
+        {
+            //implemention.  
+            return data;
+        }
+    }
+
+    public class OperationB : ISomeOperation
+    {
+        public string DoOperation(string data)
+        {
+            //implemention. 
+            return data;
+        }
+    }
+
+    public class OperationC : ISomeOperation
+    {
+        public string DoOperation(string data)
+        {
+            //implemention.  
+            return data;
+        }
+    }
+
+    public class OperationD : ISomeOperation
+    {
+        public string DoOperation(string data)
+        {
+            //implemention.  
+            return data;
+        }
+    }
+
+
+    public class OperationContext
+    {
+        private readonly Dictionary<string, ISomeOperation> _operationStrategy = new Dictionary<string, ISomeOperation>();
+
+        public OperationContext()
+        {
+            _operationStrategy.Add("name1", new OperationA());
+            _operationStrategy.Add("name2", new OperationB());
+            _operationStrategy.Add("name3", new OperationC());
+            _operationStrategy.Add("name4", new OperationD());
+        }
+
+        public string GetOperationData(string searchType, string data)
+        {
+            return _operationStrategy[searchType].DoOperation(data);
+        }
+    }
+}
